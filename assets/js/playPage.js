32d607621b5e5d22627f180dad4f1367347014bd
Kristian Cross <etamin1985@yahoo.com>
2024-09-21 18:32:22 +0000

    Add playerNotes array & add code to each event listener to ensure click key recording, add console.log to help with debugging

--- assets/js/playPage.js
+++ assets/js/playPage.js
@@ -6,6 +6,9 @@ function pickNote(note) {
     console.log(note)
 }
 
+// create the playerNotes array
+let playerNotes =[];
+
 // map piano keys to their sound files
 const keyToSoundMap = {
     'C': 'assets/music-notes/piano-c_C_major.wav',
@@ -18,36 +21,44 @@ const keyToSoundMap = {
     'C': 'assets/music-notes/piano-c_C_major.wav',
 }
 
-// add event listeners to each SVG key to ensure click functionality
+// add event listeners to each SVG key to ensure click functionality and push the key to playerNotes array 
 document.getElementById('note-c').addEventListener('click', function() {
+    playerNotes.push('C');
     playSound(keyToSoundMap['C']);
 });
 
 document.getElementById('note-d').addEventListener('click', function() {
+    playerNotes.push('D');
     playSound(keyToSoundMap['D']);
 });
 
 document.getElementById('note-e').addEventListener('click', function() {
+    playerNotes.push('E');
     playSound(keyToSoundMap['E']);
 });
 
 document.getElementById('note-f').addEventListener('click', function() {
+    playerNotes.push('F');
     playSound(keyToSoundMap['F']);
 });
 
 document.getElementById('note-g').addEventListener('click', function() {
+    playerNotes.push('G');
     playSound(keyToSoundMap['G']);
 });
 
 document.getElementById('note-a').addEventListener('click', function() {
+    playerNotes.push('A');
     playSound(keyToSoundMap['A']);
 });
 
 document.getElementById('note-b').addEventListener('click', function() {
+    playerNotes.push('B');
     playSound(keyToSoundMap['B']);
 });
 
 document.getElementById('note-c').addEventListener('click', function() {
+    playerNotes.push('C');
     playSound(keyToSoundMap['C']);
 });
     
@@ -56,6 +67,9 @@ function playSound(audioSrc) {
     const audio = new Audio(audioSrc);
     audio.play();
 }
+
+// log the playerNotes array to check the recorded notes (debugging only)
+console.log(playerNotes);
 
 const computerNotearray = ['C', 'D', 'E', 'F', 'G']; // placecholder array, this will be changed later by whoever code the generate notes system
 computerNotearray.forEach((note, index) => { // placeholder function to render the array, this will also be changed later
